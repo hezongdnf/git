d3bfc0de0b24ceb69e1aaeb3453e18de3a29efb7
hejiajin <hejiajin@aidigger.com>
2021-08-10 16:07:40 +0800

    test

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # 自己的 __name__ 在自己用时就是 main，当自己作为模块被调用时就是自己的名字
+    print(__name__)  # __main__
+    print(print_hi.__name__)  #print_hi
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
